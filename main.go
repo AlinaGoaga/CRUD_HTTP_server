7ad53a41fb3c33a8c391e554309b8575a4a66f20
Alina <goaga.alina@gmail.com>
2020-07-01 18:56:10 +0100

    add get individual book

--- main.go
+++ main.go
@@ -4,31 +4,53 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 type Book struct {
-	Title  string `json:"Title"`
-	Author string `json:"Author"`
+	Id     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
 }
-var Books []Book
+
+// For the purpose of this exercise we won't worry about persistant data so won't be using a database 
+var Books = []Book {
+	Book{Id: "1", Title: "Book1", Author: "Author1"},
+	Book{Id: "2", Title: "Book2", Author: "Author2"},
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Fprintf(w, "Welcome to the library!")
 	fmt.Println("Endpoint Hit: homePage")
 }
+
 func returnAllBooks(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllBooks")
 	json.NewEncoder(w).Encode(Books)
 }
+
+func returnSingleBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	
+	fmt.Println("Endpoint Hit: returnSingleBook")
+
+	for _, book := range Books {
+        if book.Id == id {
+            json.NewEncoder(w).Encode(book)
+        }
+	}
+}
+
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/books", returnAllBooks)
+	muxRouter := mux.NewRouter().StrictSlash(true)
+    muxRouter.HandleFunc("/", homePage)
+    muxRouter.HandleFunc("/books", returnAllBooks)
+    muxRouter.HandleFunc("/books/{id}", returnSingleBook)
 	port := ":5000"
 	fmt.Printf("Starting server on port %s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(port, muxRouter))
 }
 func main() {
-	Books = []Book{
-		Book{Title: "Book1", Author: "Author1"},
-		Book{Title: "Book2", Author: "Author2"},
-	}
 	handleRequests()
 }
